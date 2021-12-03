012082c2d1e3dd564555415777bbd1e582cde10a
shubhjitbobade <shubhajitbobade8859@gmail.com>
2021-12-03 12:27:38 +0530

    HMS project added

---
+++ myhmsdb.sql
@@ -0,0 +1,180 @@
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+--
+-- Table structure for table `admintb`
+--
+
+CREATE TABLE `admintb` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admintb`
+--
+
+INSERT INTO `admintb` (`username`, `password`) VALUES
+('admin', 'admin123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointmenttb`
+--
+
+CREATE TABLE `appointmenttb` (
+  `pid` int(11) NOT NULL,
+  `ID` int(11) NOT NULL,
+  `fname` varchar(20) NOT NULL,
+  `lname` varchar(20) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `contact` varchar(10) NOT NULL,
+  `doctor` varchar(30) NOT NULL,
+  `docFees` int(5) NOT NULL,
+  `appdate` date NOT NULL,
+  `apptime` time NOT NULL,
+  `userStatus` int(5) NOT NULL,
+  `doctorStatus` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `appointmenttb`
+--
+
+INSERT INTO `appointmenttb` (`pid`, `ID`, `fname`, `lname`, `gender`, `email`, `contact`, `doctor`, `docFees`, `appdate`, `apptime`, `userStatus`, `doctorStatus`) VALUES
+(4, 1, 'Kishan', 'Lal', 'Male', 'kishansmart0@gmail.com', '8838489464', 'Ganesh', 550, '2020-02-14', '10:00:00', 1, 0);
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `name` varchar(30) NOT NULL,
+  `email` text NOT NULL,
+  `contact` varchar(10) NOT NULL,
+  `message` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`name`, `email`, `contact`, `message`) VALUES
+('Anu', 'anu@gmail.com', '7896677554', 'Hey Admin');
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctb`
+--
+
+CREATE TABLE `doctb` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `spec` varchar(50) NOT NULL,
+  `docFees` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctb`
+--
+
+INSERT INTO `doctb` (`username`, `password`, `email`, `spec`, `docFees`) VALUES
+('ashok', 'ashok123', 'ashok@gmail.com', 'General', 500);
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patreg`
+--
+
+CREATE TABLE `patreg` (
+  `pid` int(11) NOT NULL,
+  `fname` varchar(20) NOT NULL,
+  `lname` varchar(20) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `contact` varchar(10) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `cpassword` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `patreg`
+--
+
+INSERT INTO `patreg` (`pid`, `fname`, `lname`, `gender`, `email`, `contact`, `password`, `cpassword`) VALUES
+(1, 'Ram', 'Kumar', 'Male', 'ram@gmail.com', '9876543210', 'ram123', 'ram123');
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prestb`
+--
+
+CREATE TABLE `prestb` (
+  `doctor` varchar(50) NOT NULL,
+  `pid` int(11) NOT NULL,
+  `ID` int(11) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `appdate` date NOT NULL,
+  `apptime` time NOT NULL,
+  `disease` varchar(250) NOT NULL,
+  `allergy` varchar(250) NOT NULL,
+  `prescription` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `prestb`
+--
+
+INSERT INTO `prestb` (`doctor`, `pid`, `ID`, `fname`, `lname`, `appdate`, `apptime`, `disease`, `allergy`, `prescription`) VALUES
+('Dinesh', 4, 11, 'Kishan', 'Lal', '2020-03-27', '15:00:00', 'Cough', 'Nothing', 'Just take a teaspoon of Benadryl every night');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appointmenttb`
+--
+ALTER TABLE `appointmenttb`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `patreg`
+--
+ALTER TABLE `patreg`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointmenttb`
+--
+ALTER TABLE `appointmenttb`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `patreg`
+--
+ALTER TABLE `patreg`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
